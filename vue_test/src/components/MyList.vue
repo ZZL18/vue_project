fde0e1dd19cda2a729c93c3c759881764d156601
zhouzhili <3129076109@qq.com>
2023-02-05 16:25:06 +0800

    first-vue_test

---
+++ vue_test/src/components/MyList.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul class="todo-main">
+        <MyItem></MyItem>
+        <MyItem></MyItem>
+      </ul>
+</template>
+
+<script>
+import MyItem from './MyItem.vue'
+export default {
+name:'MyList',
+components:{
+    MyItem
+}
+}
+</script>
+
+<style>
+
+</style>
